f1f849636c259d59c40878b3dbc7299024c1cd5e
roman <rleonovic98@gmail.com>
2024-12-14 15:21:30 +0400

    Initial commit: project structure and configuration

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ if __name__ == "__main__":
 
     print(get_mask_card_number(card_number))  # 7000 79** **** 6361
     print(get_mask_account(account_number))  # **4305
+
+
+
+from src.widget import mask_account_card, get_date
+
+if __name__ == "__main__":
+        # Примеры для тестирования функции mask_account_card
+        print(mask_account_card("Visa Platinum 7000792289606361"))  # Visa Platinum 7000 79** **** 6361
+        print(mask_account_card("Maestro 7000792289606361"))  # Maestro 7000 79** **** 6361
+        print(mask_account_card("Счет 73654108430135874305"))  # Счет **4305
+
+        # Примеры для тестирования функции get_date
+        print(get_date("2024-03-11T02:26:18.671407"))  # 11.03.2024
